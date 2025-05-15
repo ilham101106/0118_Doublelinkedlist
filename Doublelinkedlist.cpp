cb2cbe0db22f13771d80b01244ccb0ff0fe2ddee
ilham101106 <pesatoffical321@gmail.com>
2025-05-15 17:40:51 +0700

    fungsi retraverse untuk menampilkan data mahasiswa secara descending

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -149,4 +149,33 @@ public:
         }
     }
 
+    void revtraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        // step 1: move to last node
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        // step 2: traverse backward
+        cout << "\nRecords in descending order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            // step 3: move to previous node
+            currentNode = currentNode->prev;
+            i++;
+        }
+    }
+
     
