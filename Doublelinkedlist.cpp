7028e847a3335097a2e47b996b98e185c046b596
ilham101106 <pesatoffical321@gmail.com>
2025-05-15 17:31:52 +0700

    Step 9 : sisipkan di antara current dan current->next

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -71,4 +71,8 @@ public:
             return;
         }
 
+        // step 9: insert between current and current->next
+        newNode->next = current->next; // step 9a: newNode.next = current.next
+        newNode->prev = current;       // step 9b: newNode.prev = current
+
         
